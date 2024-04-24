aedb6277e72dd81d036b89de43a67cf430e0e71f
Marco Bravo <marco.bravoml@udlap.mx>
2024-04-23 18:20:16 -0700

    Initial app created

---
+++ src/app.R
@@ -0,0 +1,17 @@
+library(shiny)
+
+
+
+# Layout
+ui <- fluidPage(
+  theme = bslib::bs_theme(bootswatch = 'litera'),
+  h1("2020 Mexico Census"),
+  p('Shiny converts R classes into HTML'),
+  p("This conversion happens behind the scenes by Dash's JavaScript front-end"),
+)
+
+# Server side callbacks/reactivity
+server <- function(input, output, session) {}
+
+# Run the app/dashboard
+shinyApp(ui, server)
