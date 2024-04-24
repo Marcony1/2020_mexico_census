575ef02f1cfaa1d3de07d804a57d25d0f6c54a07
Marco Bravo <marco.bravoml@udlap.mx>
2024-04-23 23:51:43 -0700

    App plotting regions and some errors

--- src/app.R
+++ src/app.R
@@ -3,10 +3,11 @@ options(shiny.port = 8050, shiny.autoreload = TRUE)
 
 library(shiny)
 library(here)
+library(readr)
 library(dplyr)
 library(arrow)
 library(shinyjs)
-library(shinycssloaders) 
+library(geojsonio)
 
 
 ### Layout
@@ -33,6 +34,17 @@ ui <- fluidPage(
     choices = c(),  
     selected = NULL
   ),
+  
+  # Locality dropdown
+  selectInput(
+    "locality_dropdown",
+    "Select a locality",
+    choices = c(),
+    selected = NULL
+  ),
+  
+  # Plot area
+  plotOutput("map_plot"),
   
   br()
 
@@ -62,13 +74,19 @@ server <- function(input, output, session) {
       collect()
   })
   
+  # Import geographic information
+  geojson_file <- reactive({
+    geojsonio::geojson_read(here("data", "processed", "mexico.geojson"), what = "sp")
+  })
   
   
+  # Filtering cencus data by current state
   filtered_data <- reactive({
     req(input$state_dropdown)
     filter_df <- filter(census_dataset(), NOM_ENT == input$state_dropdown)
     return(filter_df)
   })
+  
   
   # Load states to state dropdown
   observe({
@@ -80,6 +98,7 @@ server <- function(input, output, session) {
     # Disable dropdowns
     shinyjs::disable("state_dropdown")
     shinyjs::disable("municipality_dropdown")
+    shinyjs::disable("locality_dropdown")
     
     # Waiting animation
     # showSpinner(spin = "foldingCube", color = "#337ab7")
@@ -89,10 +108,73 @@ server <- function(input, output, session) {
     # Enable dropdowns
     shinyjs::enable("state_dropdown")
     shinyjs::enable("municipality_dropdown")
+    shinyjs::enable("locality_dropdown")
     
     # Hide Waiting animation
     # hideSpinner()
     
+  })
+  
+  # Update localities dropdown based on selected state and municipality
+  observeEvent(c(input$state_dropdown, input$municipality_dropdown), {
+    # Disable dropdowns
+    shinyjs::disable("state_dropdown")
+    shinyjs::disable("municipality_dropdown")
+    shinyjs::disable("locality_dropdown")
+    
+    # Filter dataset based on selected municipality
+    filter_df <- filter(census_dataset(), NOM_ENT == input$state_dropdown,
+                                          NOM_MUN == input$municipality_dropdown)
+    
+    # Update locality dropdown choices
+    locality_choices <- unique(filter_df$NOM_LOC)
+    updateSelectInput(session, "locality_dropdown", choices = locality_choices)
+    
+    # Enable dropdowns
+    shinyjs::enable("state_dropdown")
+    shinyjs::enable("municipality_dropdown")
+    shinyjs::enable("locality_dropdown")
+  })
+  
+  
+  # Plot map
+  output$map_plot <- renderPlot({
+    
+    if (input$state_dropdown == "Total nacional"){
+      filtered_df <- geojson_file()
+    } else {
+      filtered_df <- geojson_file() |> 
+        filter(name == input$state_dropdown)
+    }
+    
+    
+    # Plot setup
+    gg <- ggplot() +
+      geom_polygon(data = filtered_df,
+                   aes(x = long,
+                       y = lat,
+                       group = group),
+                   fill = "lightgray",
+                   color = "white") +
+      theme_void() +
+      coord_map()
+    
+    coord_df <- filter(census_dataset(), 
+                       NOM_ENT == input$state_dropdown,
+                       NOM_MUN == input$municipality_dropdown,
+                       NOM_LOC == input$locality_dropdown)
+    
+  
+    if (!is.null(coord_df) && nrow(coord_df) > 0 && !is.na(coord_df$longitude_decimal) && !is.na(coord_df$latitude_decimal)) {
+      coordinates <- tibble(
+        long = coord_df$longitude_decimal[[1]],
+        lat = coord_df$latitude_decimal[[1]]
+      ) 
+      print(coordinates)
+      gg <- gg + geom_point(data = coordinates, aes(x = long, y = lat), color = "red", size = 3)
+    }
+    
+    print(gg)  
   })
   
   
